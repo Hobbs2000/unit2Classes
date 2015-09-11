e2ce6cd04485548f074f113e3ece637e30d0a11c
Hobbs2000 <cjfield@NN121-HA-06.HS.sd203.org>
2015-09-11 09:40:57 -0500

    Made a car class and made use of Java Doc

---
+++ Unit2PracitceLabs/Car.java
@@ -0,0 +1,42 @@
+
+
+/**
+ * A Car has a certain fuel efficiency and a certain amount
+ * of fuel in the gas tank. The car can be driven a certain distance
+ * 
+ * @author Calvin Field
+ * @version Version #1 9/10/15
+ */
+public class Car
+{
+    /** Fuel efficiancy of the car miles per a gallon (mpg)*/
+    private double fuelEfficiency;
+    
+    /** Amount of fuel left in the tank in gallons */
+    private double fuelTank;
+
+    /**
+     * Constructor for objects of class Car that specifies fuel efficiency
+     */
+    public Car(double fuelEfficiency)
+    {
+        // initialise instance variables
+        fuelEfficiency = fuelEfficiency;
+        fuelTank = 0;
+    }
+
+    /**
+     * Simulates driving the car a specified distance and reduces the amount of 
+     * fuel in the tank.
+     * 
+     *
+     * @pre      Specified distance cannot result in the consumption than is available
+     * @param    distance distance that the car drives in miles
+     */
+    public void drive(double distance)
+    {
+        // put your code here
+        return x+y;
+    }
+
+}
