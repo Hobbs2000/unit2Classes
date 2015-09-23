9fe3a50f4ac6c9d191eb5eddf435d8b9a69c23bc
Hobbs2000 <cjfield@NN121-HA-06.HS.sd203.org>
2015-09-23 10:30:15 -0500

    Finished the target lab

--- Unit2PracitceLabs/Target.java
+++ Unit2PracitceLabs/Target.java
@@ -22,11 +22,11 @@ public class Target
      * @param y the y-coordinate of the target
      * @param r the radius of the target
      */
-    public Target(int x, int y, int r)
+    public Target(int x, int y)
     {
         this.x = x;
         this.y = y;
-        this.radius = r;
+        this.radius = 100;
     }
 
     /**
@@ -37,21 +37,63 @@ public class Target
     {
         g2.fillOval(this.x, this.y, this.radius, this.radius);
         
+        int addY = 12;
+        int addX = 12;
         //Setup the new coordinates and dimensions for the second circle of the target
         g2.setColor(Color.white);
-        double preX = this.x * .75;
-        int addX = (int)preX;
-        int newX = this.x + addX;
+
+        int x2 = this.x + addX;
         
-        double preY = this.y * .75;
-        int addY = (int)preY;
-        int newY = this.y + addY;
+        int y2 = this.y + addY;
         
-        double preR = this.radius * .15;
+        double preR = this.radius * .25;
         int subR = (int)preR;
-        int newR = this.radius - subR;
+        int r2 = this.radius - subR;
         
-        g2.fillOval(newX, newY, newR, newR);
+        System.out.println("2nd circle x:"+x2+", y:"+y2);
+        g2.fillOval(x2, y2, r2, r2);
+        
+        //Third circle
+        g2.setColor(Color.black);
+
+        int x3 = x2 + 9;
+
+        int y3 = y2 + 9;
+        
+        preR = r2 * .25;
+        subR = (int)preR;
+        int r3 = r2 - subR;
+        
+        System.out.println("3rd circle x:"+x3+", y:"+y3);
+        g2.fillOval(x3, y3, r3, r3);
+        
+        //Fourth circle
+        g2.setColor(Color.white);
+        
+        int x4 = x3 + 13;
+        
+        int y4 = y3 + 13;
+        
+        preR = r3 * .45;
+        subR = (int)preR;
+        int r4 = r3 - subR;
+        
+        System.out.println("4th circle x:"+x4+", y:"+y4);
+        g2.fillOval(x4, y4, r4, r4);
+        
+        //Fifth circle
+        g2.setColor(Color.black);
+        
+        int x5 = x4 + 8;
+        
+        int y5 = y4 + 8;
+        
+        preR = r4 * .55;
+        subR = (int)preR;
+        int r5 = r4 - subR;
+        
+        System.out.println("5th circle x:"+x5+", y:"+y5);
+        g2.fillOval(x5, y5, r5, r5);
     }
 
 }
--- Unit2PracitceLabs/TargetComponent.java
+++ Unit2PracitceLabs/TargetComponent.java
@@ -11,7 +11,10 @@ public class TargetComponent extends JComponent
     public void paintComponent(Graphics g)
     {
         Graphics2D g2 = (Graphics2D) g;
-        Target target = new Target(10,10,100);
+        Target target = new Target(80,150);
         target.draw(g2);
+        
+        Target target2 = new Target(150, 80);
+        target2.draw(g2);
     }
 }
